61ed98318aa727286685d1ee6974f041fd4ebaaa
Rafael Lucena <104802949+Rafasluc@users.noreply.github.com>
2023-07-20 21:49:56 -0300

    Adicionando try catch

--- src/Contador.java
+++ src/Contador.java
@@ -9,7 +9,6 @@ public class Contador {
         int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
-        int resultado = 0;
 
         try {
             contar(parametroUm, parametroDois);
@@ -30,9 +29,8 @@ public class Contador {
             for (int i = 0; i < contagem; i++) {
                 System.out.println("o resultado é " + (i + 1));
             }
-        } else {
-            //System.out.println("O Segundo parâmetro deve ser maior que o primeiro");
-        }
+        }/* else {
+        }*/
     }
 
 }
